598cd3cdacc2e2bdcb1b33522d7a496286930085
Mark Lawson <mark@walkonmars.co.uk>
2022-06-01 16:17:12 +0100

    implementing JS

--- main.js
+++ main.js
@@ -1,29 +1,72 @@
 import {winningArray} from "./winningArray.js";
 
-// get the cells container
+// GET THE CELLS CONTAINER
 const cellsContainer = document.querySelector(".cells-container");
 
-// create cells
+// STARTING PLAYER
+let player = 1;
 
+// PLAYER ARRAYS
+let player1Arr = [];
+let player2Arr = [];
+
+// CREATE EMPTY CELLS
 for (let i = 0; i < 42; i++) {
-  const cells = document.createElement("div");
-  cells.classList.add("cells-container__cell");
-  cellsContainer.appendChild(cells);
+  const emptyCells = document.createElement("div");
+  emptyCells.classList.add("cells-container__cell");
+  emptyCells.setAttribute("data-id", i);
+  cellsContainer.appendChild(emptyCells);
 }
 
-const cellClicked = () => {
-  // get the value of the cell when clicked
+//  CELLS QUERY SELECTOR
+const cells = document.querySelectorAll(".cells-container__cell");
 
-  for (let i = 0; i < emptyCells.length; i++) {
-    console.log(emptyCells[i]);
+// CELL CLICKED FUNCTION
+const cellClicked = (event) => {
+  const clickedCell = event.target;
 
-    return emptyCells[i];
+  console.log(clickedCell);
+
+  const cellID = parseInt(event.target.dataset.id);
+  console.log(cellID);
+
+  for (let i = 0; i < cells.length; i++) {
+    console.log(cells[i]);
+    // if cells below yours cells[i + 7] is taken you can go on top
+
+    if (cells[i + 7].classList.contains("taken")) {
+      if (player === 1) {
+        clickedCell.classList.add("cells-container__cell--red");
+        clickedCell.classList.add("taken");
+
+        player1Arr.push(cellID);
+        console.log(player1Arr);
+
+        player = 2;
+
+        // console.log(clickedCell.dataset.id);
+      } else {
+        clickedCell.classList.add("cells-container__cell--yellow");
+
+        player2Arr.push(cellID);
+        console.log(player2Arr);
+
+        player = 1;
+        // console.log(event.target.dataset.id);
+      }
+    }
   }
+
+  // checkForWinner()
 };
 
-const emptyCells = document.querySelectorAll(".cells-container__cell");
+// const checkForWinner = () => {
+//   if(player1Arr[i].includes(winningArray[i])) {
+
+//   }
+// }
 
 // cells event listener
-emptyCells.forEach((cell) => {
+cells.forEach((cell) => {
   cell.addEventListener("click", cellClicked);
 });
