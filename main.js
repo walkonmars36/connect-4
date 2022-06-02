2d93cab13cac68b7979506566d226394d829f90e
Mark Lawson <mark@walkonmars.co.uk>
2022-06-02 14:10:54 +0100

    renaming classes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // GET THE CELLS CONTAINER
-const cellsContainer = document.querySelector(".cells-container");
+const divContainer = document.querySelector(".div-container");
 
 // STARTING PLAYER
 let player = 1;
@@ -8,54 +8,48 @@ let player = 1;
 let player1Arr = [];
 let player2Arr = [];
 
-// CREATE EMPTY CELLS
-for (let i = 0; i < 42; i++) {
-  const cells = document.createElement("div");
-  cells.classList.add("cells-container__cell");
-  cells.setAttribute("data-id", i);
-  cellsContainer.appendChild(cells);
+// CREATE DIVS
+for (let i = 0; i < 49; i++) {
+  const div = document.createElement("div");
+  div.classList.add("div-container__disc");
+  div.setAttribute("data-id", i);
+  if (i >= 42) {
+    div.className = "taken";
+  }
+  divContainer.appendChild(div);
 }
 
 //  CELLS QUERY SELECTOR
-const cellsReady = document.querySelectorAll(".cells-container__cell");
-
-// console.log(typeof cellsReady);
+const discs = document.querySelectorAll("div-container__disc");
 
 const cellClicked = (event) => {
   const clickedCell = event.target;
-
-  console.log(clickedCell);
-
+  console.log(typeof clickedCell);
   const cellID = parseInt(event.target.dataset.id);
-  console.log(cellID);
+  // console.log(cellID);
 
-  // if cellsReady[i + 7] (directly below clickedCell) is taken you can go on top
+  if (clickedCell[cellID + 7].classList.contains("taken") && !clickedCell[cellID].classList.contains("taken")) {
+    if (player === 1) {
+      clickedCell.classList.add("cells-container__cell--red");
+      clickedCell.classList.add("taken");
 
-  // if (cellsReady[i + 7].classList.contains("taken")) {
-  if (player === 1) {
-    clickedCell.classList.add("cells-container__cell--red");
-    clickedCell.classList.add("taken");
+      player1Arr.push(cellID);
+      console.log(player1Arr);
 
-    player1Arr.push(cellID);
-    console.log(player1Arr);
+      player = 2;
 
-    player = 2;
+      // console.log(clickedCell.dataset.id);
+    } else {
+      clickedCell.classList.add("cells-container__cell--yellow");
+      clickedCell.classList.add("taken");
 
-    // console.log(clickedCell.dataset.id);
-  } else {
-    clickedCell.classList.add("cells-container__cell--yellow");
-    clickedCell.classList.add("taken");
+      player2Arr.push(cellID);
+      console.log(player2Arr);
 
-    player2Arr.push(cellID);
-    console.log(player2Arr);
-
-    player = 1;
-    // console.log(event.target.dataset.id);
+      player = 1;
+      // console.log(event.target.dataset.id);
+    }
   }
-
-  // }
-
-  // checkForWinner()
 };
 
 // cells event listener
